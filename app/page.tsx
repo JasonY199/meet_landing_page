a769bf0ac6637e801c89ce4de2ebc0cfd88245a7
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-12-06 13:23:45 -0500

    layout top of mobile screen

--- app/page.tsx
+++ app/page.tsx
@@ -6,20 +6,30 @@ export default function Home() {
       <div className="mt-12 flex justify-center">
         <Image src="/logo.svg" alt="" width={118} height={28} />
       </div>
-      <div className="mx-8 mt-16 mb-8 grid grid-cols-2 gap-y-12 gap-x-[17px]">
-        <Image
-          src="/desktop/image-hero-left.png"
-          alt=""
-          width={208}
-          height={160}
-        />
-        <Image
-          src="/desktop/image-hero-right.png"
-          alt=""
-          width={208}
-          height={160}
-        />
-        <div className="col-span-2 flex flex-col gap-6">
+      <div className="mt-16 mb-8 flex flex-wrap gap-y-12">
+        <div className="overflow-hidden">
+          <div className="flex justify-center gap-[4.53vw]">
+            {/* To calculate vw units, I did image width in figma at 375px screen size, 
+            which is 208. Then 208 / 375 = 55.47%. I used the same logic for the height. */}
+            <div className="relative w-[55.47vw] h-[42.67vw] shrink-0">
+              <Image
+                src="/desktop/image-hero-left.png"
+                alt=""
+                fill
+                className="object-cover"
+              />
+            </div>
+            <div className="relative w-[55.47vw] h-[42.67vw] shrink-0">
+              <Image
+                src="/desktop/image-hero-right.png"
+                alt=""
+                fill
+                className="object-cover"
+              />
+            </div>
+          </div>
+        </div>
+        <div className="w-full mx-8 flex flex-col gap-6">
           <h1 className="text-preset-1 flex flex-col text-center">
             <span>Group Chat</span>
             <span>for Everyone</span>
