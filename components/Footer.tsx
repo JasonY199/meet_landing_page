58c551aed1f97b16a3ec9555833f7bff8e8bb0ec
Jason Y <110353075+JasonY199@users.noreply.github.com>
2025-12-24 20:37:45 -0500

    fixed footer on all screen breakpoints

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -27,17 +27,19 @@ export default function Footer() {
       <div className="absolute inset-0 bg-cyan-600 opacity-90" />
 
       {/* Content */}
-      <div className="relative h-full flex flex-col xl:flex-row items-center justify-center xl:justify-between gap-6 xl:gap-8 px-8 md:px-0 md:py-16 xl:py-0 xl:max-w-[1120px] xl:mx-auto text-center xl:text-left text-white md:max-w-[504px] md:mx-auto">
-        <div className="flex flex-col gap-6 xl:max-w-[350px]">
-          <h2 className="text-preset-2">Experience more together</h2>
-          <p className="text-preset-4">
-            Stay connected with reliable HD meetings and unlimited
-            one-on-one and group video sessions.
-          </p>
+      <div className="relative h-full flex flex-col xl:flex-row items-center justify-center gap-6 xl:justify-between px-8 md:px-0 md:py-16 xl:py-22! xl:mx-auto text-center xl:text-left text-white md:max-w-[504px] xl:max-w-[1120px]! md:mx-auto">
+        <h2 className="text-preset-2 xl:max-w-[300px]">
+          Experience more together
+        </h2>
+        <p className="text-preset-4 xl:max-w-[355px]">
+          Stay connected with reliable HD meetings and unlimited one-on-one and
+          group video sessions.
+        </p>
+        <div className="xl:self-start">
+          <Button variant="purple" version="v1.3">
+            Download
+          </Button>
         </div>
-        <Button variant="purple" version="v1.3">
-          Download
-        </Button>
       </div>
     </div>
   );
